19521c13a01bcc8287b41cc128ed46f0c257d445
hayasitakumi <b1701571@planet.kanazawa-it.ac.jp>
2020-08-18 14:25:28 +0900

    リポジトリ移行

---
+++ app/src/main/java/com/matuiken/codedbrailleblockforandroid/data/dao/TenjiDao.kt
@@ -0,0 +1,41 @@
+/**データベース操作用クラス
+ * Roomの中でデータベース操作のための機能となるDao
+ * Data Access Object
+ */
+package com.matuiken.codedbrailleblockforandroid.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.matuiken.codedbrailleblockforandroid.data.model.Blockmessage
+
+@Dao
+interface TenjiDao {
+    // blockmessage全取得
+    @Query("SELECT * FROM blockmessage WHERE code=:code AND angle=:angle AND messagecategory=:category")
+    fun getBlockmessage(code: Int, angle: Int, category: String?): Array<Blockmessage?>?
+
+    // 案内文を取得
+    @Query("SELECT message FROM blockmessage WHERE code=:code AND angle=:angle AND messagecategory=:category")
+    fun getMessage(code: Int, angle: Int, category: String?): Array<String?>?
+
+    // 音声ファイル名を取得
+    @Query("SELECT wav FROM blockmessage WHERE code=:code AND angle=:angle AND messagecategory=:category")
+    fun getWav(code: Int, angle: Int, category: String?): Array<String?>?
+
+    // blockdataにINSERTする
+    //@Query("INSERT INTO blockdata(code, category, latitude, longitude, install, buildingfloor, name) VALUES (:code, :category, :latitude, :longitude, :install, :buildingfloor, :name)")
+    //public void insertBlockdata(int code, String category, double latitude, double longitude, int install, int buildingfloor, String name);
+    // blockmessageにINSERTする（まとめてやるとき）
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertBlockmessage(vararg blockmessage: Blockmessage?)
+
+    // blockmessageにINSERTする（上と同じ、非推奨）
+    @Query("INSERT INTO blockmessage(id, code, angle, messagecategory, message, reading, wav) VALUES (:id, :code, :angle, :category, :message, :reading, :wav)")
+    fun insertBlockmessageB(id: Int, code: Int, angle: Int, category: String?, message: String?, reading: String?, wav: String?)
+
+    // blockmessageを全削除する【取扱注意】
+    @Query("DELETE FROM blockmessage")
+    fun deleteAllBlockmessage()
+}
---
+++ app/src/main/java/com/matuiken/codedbrailleblockforandroid/data/model/Blockmessage.kt
@@ -0,0 +1,28 @@
+/**ローカルデータベーステーブル定義クラス
+ * ローカルデータベースを利用するための機能Roomを使用してデータベース操作
+ * entity_blockmessage : 案内情報の管理を行うテーブル
+ */
+package com.matuiken.codedbrailleblockforandroid.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// テーブル名指定(tableName) : blockmessage   DB関連のクラスと見分けにくいのでクラス名は識別用の命名、tableNameが本命
+@Entity(tableName = "blockmessage")
+class Blockmessage {
+    @JvmField
+    @PrimaryKey
+    var id = 0
+    @JvmField
+    var code = 0
+    @JvmField
+    var angle = 0
+    @JvmField
+    var messagecategory: String? = null
+    @JvmField
+    var message: String? = null
+    @JvmField
+    var reading: String? = null
+    @JvmField
+    var wav: String? = null
+}
